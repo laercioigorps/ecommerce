2be78689016ff07e0a53c2b9a3e9bbb2db81c2f4
Laercio Souza <laercioigorps@gmail.com>
2022-11-21 17:23:42 -0300

    created ProductType model

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -12,3 +12,8 @@ class Category(models.Model):
     name = models.CharField(max_length=60)
     description = models.TextField(default="")
     parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True)
+
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(default="")
