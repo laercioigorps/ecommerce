7a30a3d3087da65126d96de99d1bb34600034ef7
Laercio Souza <laercioigorps@gmail.com>
2022-11-21 17:01:44 -0300

    added test_create_category

--- ecommerce/products/tests/test_models.py
+++ ecommerce/products/tests/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ecommerce.products.models import Brand
+from ecommerce.products.models import Brand, Category
 
 
 @pytest.mark.django_db
@@ -9,8 +9,15 @@ def test_count_brand_equals_0():
     assert brand_count == 0
 
 
-@pytest.mark.django_dbs
+@pytest.mark.django_db
 def test_create_brand():
     Brand.objects.create(name="SomeBrand", description="some description")
     brand_count = Brand.objects.all().count()
     assert brand_count == 1
+
+
+@pytest.mark.django_db
+def test_create_category():
+    Category.objects.create(name="someCategory", description="some description")
+    catogories_count = Category.objects.all().count()
+    assert catogories_count == 1
