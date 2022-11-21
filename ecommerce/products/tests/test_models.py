f0645b6a5d9397d2009bf424a83b7ca23a9ab5ed
Laercio Souza <laercioigorps@gmail.com>
2022-11-21 18:21:52 -0300

    updated tests to use class structure

--- ecommerce/products/tests/test_models.py
+++ ecommerce/products/tests/test_models.py
@@ -5,61 +5,63 @@ import pytest
 from ecommerce.products.models import Brand, Category, Colour, ProductType, Size
 
 
-@pytest.mark.django_db
-def test_count_brand_equals_0():
-    brand_count = Brand.objects.all().count()
-    assert brand_count == 0
+class TestBrand:
+    @pytest.mark.django_db
+    def test_count_brand_equals_0(self):
+        brand_count = Brand.objects.all().count()
+        assert brand_count == 0
+
+    @pytest.mark.django_db
+    def test_create_brand(self):
+        Brand.objects.create(name="SomeBrand", description="some description")
+        brand_count = Brand.objects.all().count()
+        assert brand_count == 1
 
 
-@pytest.mark.django_db
-def test_create_brand():
-    Brand.objects.create(name="SomeBrand", description="some description")
-    brand_count = Brand.objects.all().count()
-    assert brand_count == 1
+class TestCategory:
+    @pytest.mark.django_db
+    def test_create_category(self):
+        Category.objects.create(name="someCategory", description="some description")
+        catogories_count = Category.objects.all().count()
+        assert catogories_count == 1
+
+    def test_category_has_timestamped_info(self, category):
+        today = date.today()
+        assert category.created_at.date() == today
+        assert category.updated_at.date() == today
+
+    @pytest.mark.django_db
+    def test_create_category_with_parent_category(self):
+        parent = Category.objects.create(
+            name="father", description="some father description"
+        )
+        category = Category.objects.create(
+            name="children", description="some child description", parent=parent
+        )
+        catogories_count = Category.objects.all().count()
+        assert catogories_count == 2
+        assert category.parent.id == parent.id
 
 
-@pytest.mark.django_db
-def test_create_category():
-    Category.objects.create(name="someCategory", description="some description")
-    catogories_count = Category.objects.all().count()
-    assert catogories_count == 1
+class TestProductType:
+    @pytest.mark.django_db
+    def test_create_type(self):
+        ProductType.objects.create(name="some", description="some description")
+        product_type_count = ProductType.objects.all().count()
+        assert product_type_count == 1
 
 
-def test_category_has_timestamped_info(category):
-    today = date.today()
-    assert category.created_at.date() == today
-    assert category.updated_at.date() == today
+class TestProductSize:
+    @pytest.mark.django_db
+    def test_create_product_size(self):
+        Size.objects.create(name="XG", description="someDescription")
+        sizes_count = Size.objects.all().count()
+        assert sizes_count == 1
 
 
-@pytest.mark.django_db
-def test_create_category_with_parent_category():
-    parent = Category.objects.create(
-        name="father", description="some father description"
-    )
-    category = Category.objects.create(
-        name="children", description="some child description", parent=parent
-    )
-    catogories_count = Category.objects.all().count()
-    assert catogories_count == 2
-    assert category.parent.id == parent.id
-
-
-@pytest.mark.django_db
-def test_create_type():
-    ProductType.objects.create(name="some", description="some description")
-    product_type_count = ProductType.objects.all().count()
-    assert product_type_count == 1
-
-
-@pytest.mark.django_db
-def test_create_product_size():
-    Size.objects.create(name="XG", description="someDescription")
-    sizes_count = Size.objects.all().count()
-    assert sizes_count == 1
-
-
-@pytest.mark.django_db
-def test_create_product_color():
-    Colour.objects.create(name="someColour", description="some colour description")
-    colours_count = Colour.objects.all().count()
-    assert colours_count == 1
+class TestProductColour:
+    @pytest.mark.django_db
+    def test_create_product_color(self):
+        Colour.objects.create(name="someColour", description="some colour description")
+        colours_count = Colour.objects.all().count()
+        assert colours_count == 1
