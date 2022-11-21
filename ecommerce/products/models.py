8b481056b3232147bd3f4ffb626cb93da5a954a9
Laercio Souza <laercioigorps@gmail.com>
2022-11-21 16:39:30 -0300

    created Brand model

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,3 +1,8 @@
-# from django.db import models
+from django.db import models
 
 # Create your models here.
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(default="")
