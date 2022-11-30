964fe18026ceb4ec06245e36389258f5d3064e3f
Laercio Souza <laercioigorps@gmail.com>
2022-11-30 18:00:38 -0300

    added __str__ method to Brand, Category, ProductType, Size, Colour, Product, and SubProduct

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -9,26 +9,41 @@ class Brand(TimeStampedModel):
     name = models.CharField(max_length=60)
     description = models.TextField(default="")
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Category(TimeStampedModel):
     name = models.CharField(max_length=60)
     description = models.TextField(default="")
     parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class ProductType(TimeStampedModel):
     name = models.CharField(max_length=60)
     description = models.TextField(default="")
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Size(TimeStampedModel):
     name = models.CharField(max_length=60)
     description = models.TextField(default="")
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Colour(TimeStampedModel):
     name = models.CharField(max_length=30)
     description = models.TextField(default="")
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Product(TimeStampedModel):
@@ -47,6 +62,9 @@ class Product(TimeStampedModel):
     genre = models.CharField(max_length=20, choices=GENRE_CHOICES, null=True)
     usage = models.CharField(max_length=30)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class SubProduct(TimeStampedModel):
     SKU = models.CharField(max_length=40, unique=True)
@@ -56,6 +74,11 @@ class SubProduct(TimeStampedModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     colour = models.ForeignKey(Colour, on_delete=models.CASCADE, null=True)
     size = models.ForeignKey(Size, on_delete=models.CASCADE, null=True)
+
+    def __str__(self) -> str:
+        return "{} - {} - {}".format(
+            self.product.name, self.colour.name, self.size.name
+        )
 
 
 class Media(models.Model):
