5afc421212fc715dee187c0f4734d897dc644edc
Laercio Souza <laercioigorps@gmail.com>
2022-11-30 17:45:40 -0300

    registered products models in admin site

--- ecommerce/products/admin.py
+++ ecommerce/products/admin.py
@@ -1,3 +1,13 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from .models import Brand, Category, Colour, Media, Product, Size, SubProduct
 
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(SubProduct)
+admin.site.register(Size)
+admin.site.register(Brand)
+admin.site.register(Category)
+admin.site.register(Colour)
+admin.site.register(Media)
