d019280faeddd6d1ecc7ea578d5688bd58d82731
Laercio Souza <laercioigorps@gmail.com>
2022-12-16 18:24:14 -0300

    added product_with_many_sub_products fixture

--- ecommerce/conftest.py
+++ ecommerce/conftest.py
@@ -81,3 +81,52 @@ def media(db, product):
         alt_text="what is happening in the image",
         product=product,
     )
+
+
+@pytest.fixture
+def product_with_many_sub_products(db, product):
+    colour1 = Colour.objects.create(name="colour 1", description="")
+    colour2 = Colour.objects.create(name="colour 2", description="")
+    colour3 = Colour.objects.create(name="colour 3", description="")
+
+    size1 = Size.objects.create(name="MD")
+    size2 = Size.objects.create(name="LG")
+    size3 = Size.objects.create(name="XL")
+    subProducts = []
+    subProducts.append(
+        SubProduct(
+            SKU="XYZ123456",
+            rr_price=99.90,
+            store_price=150.99,
+            sale_price=120.00,
+            product=product,
+            colour=colour1,
+            size=size1,
+        )
+    )
+    subProducts.append(
+        SubProduct(
+            SKU="XYZ123455",
+            rr_price=90.90,
+            store_price=160.25,
+            sale_price=130.00,
+            product=product,
+            colour=colour2,
+            size=size2,
+        )
+    )
+    subProducts.append(
+        SubProduct(
+            SKU="XYZ123454",
+            rr_price=100.90,
+            store_price=161.25,
+            sale_price=132.00,
+            product=product,
+            colour=colour3,
+            size=size3,
+        )
+    )
+
+    SubProduct.objects.bulk_create(subProducts)
+
+    return product
