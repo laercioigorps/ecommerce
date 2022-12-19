dc2004ab1c1883590cf4d09422b8426b31fb70b4
Laercio Souza <laercioigorps@gmail.com>
2022-12-19 17:31:09 -0300

    updated ShoppingCartView post method to use active shopping cart if exists

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -14,7 +14,12 @@ class ShoppingCartView(View):
     def post(self, request):
         item = request.POST.get("item")
         quantity = request.POST.get("quantity")
+
         subproduct = get_object_or_404(SubProduct, pk=item)
-        shoppingcart = ShoppingCart.objects.create(owner=request.user)
+        carts = ShoppingCart.objects.filter(owner=request.user).filter(is_active=True)
+        if not carts:
+            shoppingcart = ShoppingCart.objects.create(owner=request.user)
+        else:
+            shoppingcart = carts.first()
         shoppingcart.items.add(subproduct, through_defaults={"quantity": int(quantity)})
         return HttpResponse(status=200)
