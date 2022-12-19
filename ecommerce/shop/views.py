c18f063c307fbb9e9a8ccc25a8cb9c7dc3f5ae86
Laercio Souza <laercioigorps@gmail.com>
2022-12-19 15:42:55 -0300

    created ShoppingCartView and added post method to add items to shopping cart

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,3 +1,20 @@
 # from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.views import View
+
+from ecommerce.products.models import SubProduct
+
+from .models import ShoppingCart
 
 # Create your views here.
+
+
+class ShoppingCartView(View):
+    def post(self, request):
+        item = request.POST.get("item")
+        quantity = request.POST.get("quantity")
+        subproduct = get_object_or_404(SubProduct, pk=item)
+        shoppingcart = ShoppingCart.objects.create(owner=request.user)
+        shoppingcart.items.add(subproduct, through_defaults={"quantity": int(quantity)})
+        return HttpResponse(status=200)
