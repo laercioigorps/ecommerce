7a0baac0883bbdf0d271708e1071dfb7597a1b2c
Laercio Souza <laercioigorps@gmail.com>
2022-12-20 15:15:07 -0300

    updated ShoppingCartView post method to use ShoppingCartServices.add_or_update_cart_item

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -18,15 +18,5 @@ class ShoppingCartView(View):
         subproduct = get_object_or_404(SubProduct, pk=item)
         shoppingcart = ShoppingCartServices.get_active_or_create(request.user)
         # checks if the item already exists in the cart
-        is_cart_item = shoppingcart.items.all().filter(pk=subproduct.id).exists()
-        if not is_cart_item:
-            # add item
-            shoppingcart.items.add(
-                subproduct, through_defaults={"quantity": int(quantity)}
-            )
-        else:
-            # updates the item quantity
-            shopping_cart_item = shoppingcart.shoppingcartitem_set.get(item=subproduct)
-            shopping_cart_item.quantity += int(quantity)
-            shopping_cart_item.save()
+        ShoppingCartServices.add_or_update_cart_item(shoppingcart, subproduct, quantity)
         return HttpResponse(status=200)
