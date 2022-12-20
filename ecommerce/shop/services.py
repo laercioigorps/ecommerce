aaae4162f2661eb6f183971ad6c71e79260f9b7c
Laercio Souza <laercioigorps@gmail.com>
2022-12-20 15:12:59 -0300

    added add_or_update_cart_item to ShoppingCartServices

--- ecommerce/shop/services.py
+++ ecommerce/shop/services.py
@@ -9,3 +9,14 @@ class ShoppingCartServices:
         else:
             shoppingcart = carts.first()
         return shoppingcart
+
+    def add_or_update_cart_item(cart, item, quantity):
+        is_cart_item = cart.items.all().filter(pk=item.id).exists()
+        if not is_cart_item:
+            # add item
+            cart.items.add(item, through_defaults={"quantity": int(quantity)})
+        else:
+            # updates the item quantity
+            shopping_cart_item = cart.shoppingcartitem_set.get(item=item)
+            shopping_cart_item.quantity += int(quantity)
+            shopping_cart_item.save()
--- ecommerce/shop/tests/test_services.py
+++ ecommerce/shop/tests/test_services.py
@@ -24,3 +24,24 @@ def test_get_active_shopping_cart_or_create_should_not_create(user):
     shopping_cart_count = ShoppingCart.objects.count()
     assert shopping_cart.owner == user
     assert shopping_cart_count == 1
+
+
+@pytest.mark.django_db
+def test_add_or_update_cart_item_should_add(user, subProduct):
+    shopping_cart = ShoppingCartServices.get_active_or_create(owner=user)
+    assert shopping_cart.items.count() == 0
+    ShoppingCartServices.add_or_update_cart_item(shopping_cart, subProduct, 3)
+    shopping_cart_item = shopping_cart.shoppingcartitem_set.get(item=subProduct)
+    assert shopping_cart.items.count() == 1
+    assert shopping_cart_item.quantity == 3
+
+
+@pytest.mark.django_db
+def test_add_or_update_cart_item_should_update(user, subProduct):
+    shopping_cart = ShoppingCartServices.get_active_or_create(owner=user)
+    ShoppingCartServices.add_or_update_cart_item(shopping_cart, subProduct, 3)
+    ShoppingCartServices.add_or_update_cart_item(shopping_cart, subProduct, 3)
+
+    shopping_cart_item = shopping_cart.shoppingcartitem_set.get(item=subProduct)
+    assert shopping_cart.items.count() == 1
+    assert shopping_cart_item.quantity == 6
