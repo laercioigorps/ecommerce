b08895bb02df4a23b4414708c47e980716d4fb33
Laercio Souza <laercioigorps@gmail.com>
2023-01-30 16:39:14 -0300

    refactores test views

--- ecommerce/shop/tests/test_views.py
+++ ecommerce/shop/tests/test_views.py
@@ -4,7 +4,6 @@ from pytest_django.asserts import assertRedirects, assertTemplateUsed
 
 from ecommerce.products.tests.factories import SubProductFactory
 from ecommerce.shop.models import ShoppingCart
-from ecommerce.shop.views import ShoppingCartAddItemView, ShoppingCartRemoveItemView
 
 from ..services import ShoppingCartServices
 
@@ -23,53 +22,37 @@ def test_shopping_cart_page_with_logged_user(user, client, subProduct):
     assert response.context["items"][0].quantity == 3
 
 
-def test_add_invalid_item_to_shopping_cart_view(user, rf):
-    request = rf.post("/random/", {"item": 999, "quantity": 2})
-    request.user = user
-
-    view = ShoppingCartAddItemView.as_view()
-    with pytest.raises(Exception):
-        view(request)
+@pytest.mark.django_db
+def test_add_invalid_item_to_shopping_cart_view(client):
+    response = client.post(reverse("shop:add_to_cart"), {"item": 999, "quantity": 2})
+    assert response.status_code == 404
 
 
-def test_add_item_to_shopping_cart_with_valid_user_view(user, rf, subProduct):
-    request = rf.post("/random/", {"item": subProduct.id, "quantity": 2})
-    request.user = user
-    view = ShoppingCartAddItemView.as_view()
-    response = view(request)
+@pytest.mark.django_db
+def test_add_item_to_shopping_cart_with_valid_user_view(user, client, subProduct):
+    client.force_login(user)
+    response = client.post(
+        reverse("shop:add_to_cart"), {"item": subProduct.id, "quantity": 2}
+    )
     cart_item_count = subProduct.shoppingcartitem_set.count()
     cart_item = subProduct.shoppingcartitem_set.first()
 
-    assert response.status_code == 302
+    assertRedirects(response, reverse("shop:cart_page"))
     assert cart_item_count == 1
     assert cart_item.quantity == 2
     assert cart_item.cart.owner == user
 
 
-def test_add_item_to_shopping_cart_with_valid_user_view_redirects_to_cart_page(
-    user, client, subProduct
-):
+@pytest.mark.django_db
+def test_add_two_items_to_same_shopping_cart(user, client):
     client.force_login(user)
-    response = client.post(
-        reverse("shop:add_to_cart"), {"item": subProduct.id, "quantity": 2}
-    )
-    assertRedirects(response, reverse("shop:cart_page"))
-
-
-def test_add_two_items_to_same_shopping_cart(user, rf):
     item1 = SubProductFactory()
     item2 = SubProductFactory()
 
-    view = ShoppingCartAddItemView.as_view()
-
     # add first item
-    request = rf.post("/random/", {"item": item1.id, "quantity": 2})
-    request.user = user
-    view(request)
+    client.post(reverse("shop:add_to_cart"), {"item": item1.id, "quantity": 2})
     # add second item
-    request = rf.post("/random/", {"item": item2.id, "quantity": 3})
-    request.user = user
-    view(request)
+    client.post(reverse("shop:add_to_cart"), {"item": item2.id, "quantity": 3})
 
     shopping_cart_count = ShoppingCart.objects.all().count()
     shopping_cart = ShoppingCart.objects.first()
@@ -81,17 +64,13 @@ def test_add_two_items_to_same_shopping_cart(user, rf):
     assert shopping_cart_items[1].id == item2.id
 
 
-def test_add_same_item_twice_to_shopping_cart(user, rf, subProduct):
-    view = ShoppingCartAddItemView.as_view()
-
+@pytest.mark.django_db
+def test_add_same_item_twice_to_shopping_cart(user, client, subProduct):
+    client.force_login(user)
     # add first item
-    request = rf.post("/random/", {"item": subProduct.id, "quantity": 2})
-    request.user = user
-    view(request)
+    client.post(reverse("shop:add_to_cart"), {"item": subProduct.id, "quantity": 2})
     # add second item
-    request = rf.post("/random/", {"item": subProduct.id, "quantity": 3})
-    request.user = user
-    view(request)
+    client.post(reverse("shop:add_to_cart"), {"item": subProduct.id, "quantity": 3})
 
     shopping_cart = ShoppingCart.objects.first()
     shopping_cart_items = shopping_cart.items.all()
@@ -102,17 +81,15 @@ def test_add_same_item_twice_to_shopping_cart(user, rf, subProduct):
     assert shopping_cart_item.quantity == 5
 
 
-def test_delete_shopping_cart_item(user, rf, subProduct):
-    view = ShoppingCartAddItemView.as_view()
+@pytest.mark.django_db
+def test_delete_shopping_cart_item(user, client, subProduct):
+    client.force_login(user)
     # add first item
-    request = rf.post("/random/", {"item": subProduct.id, "quantity": 2})
-    request.user = user
-    response = view(request)
+    response = client.post(
+        reverse("shop:add_to_cart"), {"item": subProduct.id, "quantity": 2}
+    )
     # add second item
-    view = ShoppingCartRemoveItemView.as_view()
-    request = rf.post("/random/")
-    request.user = user
-    response = view(request, subProduct.id)
+    response = client.post(reverse("shop:remove_from_cart", args=[subProduct.id]))
 
     shopping_cart = ShoppingCart.objects.first()
     shopping_cart_items = shopping_cart.items.all()
@@ -121,6 +98,7 @@ def test_delete_shopping_cart_item(user, rf, subProduct):
     assert shopping_cart_items.count() == 0
 
 
+@pytest.mark.django_db
 def test_delete_item_from_shopping_cart_with_valid_user_view_redirects_to_cart_page(
     user, client, subProduct
 ):
@@ -137,15 +115,11 @@ def test_delete_item_from_shopping_cart_with_valid_user_view_redirects_to_cart_p
     assertRedirects(response, reverse("shop:cart_page"))
 
 
-def test_delete_invelid_shopping_cart_item(user, rf, subProduct):
+@pytest.mark.django_db
+def test_delete_invalid_shopping_cart_item(user, client, subProduct):
     # add second item
-    view = ShoppingCartRemoveItemView.as_view()
-    request = rf.post("/random/")
-    request.user = user
-    response = view(request, subProduct.id)
-
-    shopping_cart = ShoppingCart.objects.first()
-    shopping_cart_items = shopping_cart.items.all()
-
+    client.force_login(user)
+    response = client.post(
+        reverse("shop:remove_from_cart", kwargs={"item": 99}),
+    )
     assert response.status_code == 404
-    assert shopping_cart_items.count() == 0
